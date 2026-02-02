e53bbf62dbf47c3e6078ca9a7a62d8304a2d9828
natalia <ponomarevanatasha16072007@gmail.com>
2026-02-02 03:20:05 +0300

    логика продолжения

--- backend/program.cs
+++ backend/program.cs
@@ -9,6 +9,27 @@ using System;
 
         Console.WriteLine($"Сегодня {DateTime.Now.DayOfWeek}");
 
-        
+        bool продолжить = true;
+
+        while (продолжить)
+        {
+            Console.WriteLine("Хотите продолжить? (y/n)");
+            string ответ = Console.ReadLine();
+
+            if (ответ.ToLower() == "n")
+            {
+                продолжить = false;
+            }
+            else if (ответ.ToLower() == "y")
+            {
+                Console.WriteLine("Продолжение…");
+                Console.WriteLine($"Привет, {имя}!");
+                Console.WriteLine($"Сегодня {DateTime.Now.DayOfWeek}");
+            }
+            else
+            {
+                Console.WriteLine("Пожалуйста, введите y или n");
+            }
+        }
     }
 }
