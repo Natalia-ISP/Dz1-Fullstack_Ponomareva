5a80d2940cc833af0255d19a8e40d338819d7920
natalia <ponomarevanatasha16072007@gmail.com>
2026-02-02 03:14:24 +0300

    ввод имени

---
+++ backend/program.cs
@@ -0,0 +1,10 @@
+using System;
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите ваше имя:");
+        string имя = Console.ReadLine();
+
+        Console.WriteLine($"Привет, {имя}!");
+    }
+}
